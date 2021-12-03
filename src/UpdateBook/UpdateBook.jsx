256c104d52d28ebc301970573c71b59b17fc52dc
Zebiniso <72644178+santiill@users.noreply.github.com>
2021-12-03 09:32:39 +0600

    Update UpdateBook.jsx

--- src/UpdateBook/UpdateBook.jsx
+++ src/UpdateBook/UpdateBook.jsx
@@ -6,8 +6,8 @@ export const UpdateBook = ({id}) => {
 
     const submitHandler = (id) => {
             axios.put(`http://localhost:4000/books/${id}`, {
-                title: title,
-                author: author
+                title: userState.title,
+                author: userState.author
             })
             .then(function (response) {
                 console.log(response);
@@ -17,31 +17,38 @@ export const UpdateBook = ({id}) => {
             });
         }
 
-    const [title, setTitle] = useState("")
-    const [author, setAuthor] = useState("")
-
-    const changeHandler = (e) => {
-        setTitle(e.target.value)
-        setAuthor(e.target.value)
+    const [userState, setUserState] = useState({
+        title: "",
+        author: ""
+        })
+        
+    const changeHandler = (e)=> {
+        const value = e.target.value;
+        setUserState({
+        ...userState,
+        [e.target.name]: value
+        });
     }
 
     return (
-        <><Flex>
-            <h3>Update Book</h3>
-            <form onSubmit={()=>submitHandler(id)}>
-                <Flex>
-                    <div>
-                        <label htmlFor="title">Title: </label>
-                        <input id="title" type="text" name="author" value={author} onChange={changeHandler}/>
-                    </div>
-                    <div>
-                        <label htmlFor="author">Author: </label>
-                        <input id="author" type="text" name="title" value={title} onChange={changeHandler}/>
-                    </div>
-                    <button type="submit">Edit</button>
-                </Flex>
-                <hr style={{height:"1px", background:"black"}} />
-            </form></Flex>
+        <>
+            <Flex>
+                <h3>Update Book</h3>
+                <form onSubmit={()=>submitHandler(id)}>
+                    <Flex>
+                        <div>
+                            <label htmlFor="title">Title: </label>
+                            <input id="title" type="text" name="author" value={userState.author} onChange={changeHandler}/>
+                        </div>
+                        <div>
+                            <label htmlFor="author">Author: </label>
+                            <input id="author" type="text" name="title" value={userState.title} onChange={changeHandler}/>
+                        </div>
+                        <button type="submit">Edit</button>
+                    </Flex>
+                    <hr style={{height:"1px", background:"black"}} />
+                </form>
+            </Flex>
         </>
     )
 }
